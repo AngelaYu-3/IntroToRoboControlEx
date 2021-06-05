35863cf4badb37cacd2c5b00ef722d5d3ee450dd
AngelaYu-3 <55929781+AngelaYu-3@users.noreply.github.com>
2021-06-05 14:19:40 -0700

    updated ports

--- IntroToRobotControlBase/src/main/java/frc/robot/subsystems/RobotMap.java
+++ IntroToRobotControlBase/src/main/java/frc/robot/subsystems/RobotMap.java
@@ -3,8 +3,8 @@ package frc.robot.subsystems;
 public class RobotMap {
 
     //talon port numbers can be confirmed in PhoenixTuner
-    public static int leftDrivePort = 0;
-    public static int rightDrivePort = 1;
+    public static int leftDrivePort = 1;
+    public static int rightDrivePort = 2;
 
     //joystick port numbers can be confirmed in DriverStation
     public static int joyPort = 0;
